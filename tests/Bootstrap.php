743c2248b2936df9e296d302417080c8b152ada0
Arie Timmerman <arietimmerman@gmail.com>
2020-12-24 13:39:19 +0100

    Feature/pipeline fix (#6)
    chmod test private key to 0600

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+chmod(__DIR__ . '/../vendor/league/oauth2-server/tests/Stubs/private.key', 0600);
+
 if (!@include_once __DIR__ . '/../vendor/autoload.php') {
     $message = <<<MSG
 You must set up the project dependencies, run the following commands:
